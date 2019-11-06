d6062b1b2acba436603667f3f00706d66c7fc3af
DanielHee59 <47747846+DanielHee59@users.noreply.github.com>
2019-11-06 13:29:13 +0800

    Initial commit

---
+++ PipeClient/PipeClient/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace PipeClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbReceiveServer = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username: ";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(108, 64);
+            this.tbUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(153, 22);
+            this.tbUsername.TabIndex = 1;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(108, 96);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(153, 22);
+            this.tbPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password: ";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(23, 145);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 28);
+            this.btnLogin.TabIndex = 14;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(436, 239);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(248, 53);
+            this.btnConnect.TabIndex = 15;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(436, 299);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(248, 53);
+            this.btnDisconnect.TabIndex = 16;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 230);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Receiving from Server:";
+            // 
+            // tbReceiveServer
+            // 
+            this.tbReceiveServer.Location = new System.Drawing.Point(13, 264);
+            this.tbReceiveServer.Margin = new System.Windows.Forms.Padding(4);
+            this.tbReceiveServer.Multiline = true;
+            this.tbReceiveServer.Name = "tbReceiveServer";
+            this.tbReceiveServer.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbReceiveServer.Size = new System.Drawing.Size(328, 186);
+            this.tbReceiveServer.TabIndex = 19;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(23, 482);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 28);
+            this.btnReset.TabIndex = 20;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(436, 184);
+            this.tbPath.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(153, 22);
+            this.tbPath.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(433, 151);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 17);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 554);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.tbReceiveServer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbReceiveServer;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PipeClient/PipeClient/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AT_4;
+
+namespace PipeClient
+{
+
+    //Name: Daniel Hee
+    //Student ID: P466436
+    //Programming AT 2.4
+    public partial class Form1 : Form
+    {
+        private PipeClient PipeClient = new PipeClient();
+
+        string path = "\\\\.\\pipe\\123123"; //path location for client to connect to server
+
+        // Dummy repository class for DB operations
+        MockUserRepository userRepo = new MockUserRepository();
+        // Let us use the Password manager class to generate the password ans salt
+        PasswordManager pwdManager = new PasswordManager();
+
+        public Form1()
+        {
+            InitializeComponent();
+            PipeClient.MessageReceived += pipeClient_MessageReceived;
+            PipeClient.ServerDisconnected += pipeClient_ServerDisconnected;
+            defaultUser();
+            tbPath.Text = path;                   
+        }
+
+        //Add a default user to user database
+        private void defaultUser()
+        {
+            string userid = "abcde";
+            string password = "abcde";
+            string salt = null;
+            string passwordHash = pwdManager.GeneratePasswordHash(password, out salt);
+            // save the values in the database
+            User user = new User
+            {
+                UserId = userid,
+                PasswordHash = passwordHash,
+                Salt = salt
+            };
+            userRepo.AddUser(user); //Add user information into database
+        }
+
+        void pipeClient_ServerDisconnected()
+        {
+            Invoke(new PipeClient.ServerDisconnectedHandler(EnableStartButton));
+        }
+
+        //If client is disconnected from server, connect button will change from false to true
+        void EnableStartButton()
+        {
+            btnConnect.Enabled = true;
+        }
+
+        void pipeClient_MessageReceived(byte[] message)
+        {
+            Invoke(new PipeClient.MessageReceivedHandler(DisplayReceivedMessage),
+                new object[] { message });
+        }
+
+        //Message sent from Server
+        void DisplayReceivedMessage(byte[] message)
+        {
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            string str = encoder.GetString(message, 0, message.Length); //Decrypt receiving data(Server) from bytes to string
+
+            tbReceiveServer.Text += str;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            PipeClient.Connect(path); //Connect the path shown at TextBox with the Label Path
+
+            if (!PipeClient.Connected) //if its not connected to server, display an error message
+            {
+                MessageBox.Show("Connection Error","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                btnConnect.Enabled = false; //if its connected to server, set button to false 
+            }
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //if both textBox is empty or one of them is empty, display an error "Missing Data"
+            //if both textBox is filled with data then encode it into Bytes and send it to the server for validation
+            if(!string.IsNullOrEmpty(tbUsername.Text) && !string.IsNullOrEmpty(tbPassword.Text))
+            {
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                PipeClient.SendMessage(encoder.GetBytes(tbUsername.Text + "," + tbPassword.Text));
+            }
+            else
+            {
+                MessageBox.Show("Missing Data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // if button is clear, client will be disconnected from server 
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            PipeClient.Disconnect();
+            btnConnect.Enabled = true;
+        }
+
+        //Clear all items found in the textBox called tbReceiveServer
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            tbReceiveServer.Clear();
+        }
+
+        
+    }
+}
